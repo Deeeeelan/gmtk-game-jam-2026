5c08a55e354c8c4575587153c73598d44b1b50b6
FrankT1 <sixingt123@gmail.com>
2026-07-25 15:47:33 -0700

    Add stuff again

--- scripts/minigames/mg_sign.gd
+++ scripts/minigames/mg_sign.gd
@@ -2,7 +2,18 @@ extends Control
 
 @onready var game_manager: Node = GameManager
 
+var drawable_columns = 64
+var drawable_rows = 128
+
 func play() -> void:
+	#$ColorRect.gui_input.connect(func(event: InputEvent):
+		#if event is InputEventMouseButton:
+			#if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				#print("hi world")
+	#)
+	pass
+	
+func createDrawableArea(location:Vector2) -> void:
 	pass
 		
 func _ready() -> void:
