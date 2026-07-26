7ed0466d3d259fe813b91d466d8e91011e96e670
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-07-25 23:41:27 -0700

    Fix windows

--- scripts/game_loop_manager.gd
+++ scripts/game_loop_manager.gd
@@ -24,6 +24,7 @@ const RANKS = [
 ]
 
 func promote():
+	promotion
 	promotion.modulate = Color(0.0, 0.0, 0.0, 0.0)
 	var tween = get_tree().create_tween()
 	tween.tween_property(promotion, "modulate", Color(1.0, 1.0, 1.0, 1.0), 1.0)
