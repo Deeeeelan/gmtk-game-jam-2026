a8c1e94829cfce9b86341479f2683755e43394af
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-07-26 00:03:22 -0700

    Add shutdown sfx

--- scenes/shutdown.gd
+++ scenes/shutdown.gd
@@ -1,4 +1,10 @@
 extends Button
 
 func _pressed() -> void:
+	%SFX_shutdown.play()
+	%Fade.color = Color(0.0, 0.0, 0.0, 0.0)
+	var tween = get_tree().create_tween()
+	tween.tween_property(%Fade, "color", Color(0.0, 0.0, 0.0, 1.0), 1.0)
+	tween.play()
+	await get_tree().create_timer(3).timeout
 	GameManager.shutdown()
