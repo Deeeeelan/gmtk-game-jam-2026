572783e574551e87e30a3fec6fa2eaef95e62f6f
FrankT1 <sixingt123@gmail.com>
2026-07-25 20:53:09 -0700

    Add a bunch of stuff i dont even know

--- scripts/minigames/mg_sign.gd
+++ scripts/minigames/mg_sign.gd
@@ -3,29 +3,56 @@ extends Control
 @onready var game_manager: Node = GameManager
 @onready var canvas: GridContainer = $Canvas
 
-var drawable_columns = 64
-var drawable_rows = 32
-var is_drawing = false
+var doc_info: Dictionary = {
+	1: [{
+			"loc": Vector2(385, 370), "size": Vector2(50, 14)}],
+	2: [{
+			"loc": Vector2(385, 370), "size": Vector2(10, 10)},
+		{
+			"loc": Vector2(100, 100), "size": Vector2(15, 15)}],
+	3: [{
+			"loc": Vector2(), "size": Vector2()},
+		{
+			"loc": Vector2(), "size": Vector2()},
+		{
+			"loc": Vector2(), "size": Vector2()}]}
+
+var filled: Dictionary = {
+	1: true,
+	2: true,
+	3: true
+}
+
+var is_drawing: bool = false
+var canvas_size = Vector2(2, 2)
+var canvases = []
+
+var doc_count: int = 10
 
 func play() -> void:
-	#$ColorRect.gui_input.connect(func(event: InputEvent):
-		#if event is InputEventMouseButton:
-			#if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-				#print("hi world")
-	#)
-	pass
+	while doc_count > 0:
+		var current_doc = 1#(randi() % 3) + 1
+		for i in doc_info[current_doc]:
+			createDrawableArea(i["loc"], i["size"])
+		
+		
+		doc_count -= 1
+		break #debug
 	
-func createDrawableArea() -> void:
-	canvas.columns = drawable_columns
-	
-	for i in range(drawable_columns * drawable_rows):
+func createDrawableArea(location: Vector2i, dimentions: Vector2i) -> void:
+	var canv = canvas.duplicate()
+	add_child(canv)
+	canvases.append(canv)
+	canv.global_position = location
+	canv.columns = dimentions.x
+	for i in range(dimentions.x * dimentions.y):
 		var cell = TextureButton.new()
 		cell.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
-		cell.custom_minimum_size = Vector2(4,4)
+		cell.custom_minimum_size = canvas_size
 		cell.stretch_mode = TextureButton.STRETCH_SCALE
 		cell.ignore_texture_size = true
 		cell.texture_normal = load("res://assets/images/misc/white1x1pixel.png")
-		canvas.add_child(cell)
+		canv.add_child(cell)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -36,6 +63,7 @@ func _input(event: InputEvent) -> void:
 			cell_at_mouse.modulate = Color(0, 0, 0)
 		if event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
 			is_drawing = false
+			
 		
 	if event is InputEventMouseMotion and is_drawing:
 		var cell_at_mouse = getCellAtMouse(event.position)
@@ -43,14 +71,14 @@ func _input(event: InputEvent) -> void:
 			cell_at_mouse.modulate = Color(0, 0, 0)
 
 func getCellAtMouse(mouse_pos: Vector2) -> TextureButton:
-	var canvas_buttons = canvas.get_children()
-	for i in canvas_buttons:
-		var button_pos = i.global_position
-		var button_size = i.custom_minimum_size
-		if button_pos.x <= mouse_pos.x and button_pos.x + button_size.x >= mouse_pos.x and button_pos.y <= mouse_pos.y and button_pos.y + button_size.y >= mouse_pos.y:
-			return i
+	for canv in canvases:
+		var canvas_buttons = canv.get_children()
+		for i in canvas_buttons:
+			var button_pos = i.global_position
+			var button_size = i.custom_minimum_size
+			if button_pos.x <= mouse_pos.x and button_pos.x + button_size.x >= mouse_pos.x and button_pos.y <= mouse_pos.y and button_pos.y + button_size.y >= mouse_pos.y:
+				return i
 	return null
 
 func _ready() -> void:
-	createDrawableArea()
 	play()
